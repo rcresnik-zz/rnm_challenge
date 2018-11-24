a7e12ed948bd847048027d7c5f35a7467376cb68
Rok Cresnik <rok@cresnik.com>
2018-11-24 08:15:59 +0100

    initial commit.

---
+++ rnm_challenge/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  rnm_challenge
+//
+//  Created by rokit on 24/11/2018.
+//  Copyright © 2018 rok cresnik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
