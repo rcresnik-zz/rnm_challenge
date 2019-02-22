ad5085a9a926000127a97d172d34fd65253df0ba
Rok Cresnik <rok@cresnik.com>
2019-02-22 16:57:15 +0100

    Update UI tests.

--- rnm_challengeUITests/Characters/FavoritesTests.swift
+++ rnm_challengeUITests/Characters/FavoritesTests.swift
@@ -9,17 +9,12 @@
 import XCTest
 
 class rnm_challengeUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launchArguments.append("uitesting")
+        app.launch()
     }
 
     override func tearDown() {
@@ -28,8 +23,6 @@ class rnm_challengeUITests: XCTestCase {
 
     func testAddFavorites() {
         let name = "Morty Smith"
-        let app = XCUIApplication()
-        app.launchArguments.append("uitesting")
 
         app.tables.staticTexts[name].tap()
 
@@ -44,7 +37,6 @@ class rnm_challengeUITests: XCTestCase {
 
     func testRemoveFavorites() {
         let name = "Summer Smith"
-        let app = XCUIApplication()
 
         app.tables.staticTexts[name].tap()
 
--- rnm_challengeUITests/Location/LocationTests.swift
+++ rnm_challengeUITests/Location/LocationTests.swift
@@ -9,17 +9,12 @@
 import XCTest
 
 class LocationTests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launchArguments.append("uitesting")
+        app.launch()
     }
 
     override func tearDown() {
@@ -28,8 +23,6 @@ class LocationTests: XCTestCase {
 
     func testOrigin() {
         let name = "Jerry Smith"
-
-        let app = XCUIApplication()
 
         app.tables.staticTexts[name].tap()
         sleep(1)
@@ -48,9 +41,7 @@ class LocationTests: XCTestCase {
     func testLastLocation() {
         let name = "Adjudicator Rick"
 
-        let app = XCUIApplication()
         app.swipeUp()
-
         app.tables.staticTexts[name].firstMatch.tap()
         sleep(1)
 
